dcafdc5c0f4f4819ab3a19c6347a880df2f4e92b
marton bognar <martonbognar@sch.bme.hu>
2019-09-08 14:06:39 +0200

    Use string representation of the equation

--- main.pl
+++ main.pl
@@ -4,13 +4,8 @@
 :- use_module(solver).
 
 go :- 
-    solver:equation([
-        [a("Mo", 1), a("O", 3)],                                                                                                                                  
-        [m([a("C", 4), a("H", 9)], 4), a("N", 1), a("O", 1), a("H", 1)]
-    ], [        
-        [m([m([a("C", 4), a("H", 9)], 4), a("N", 1)], 4), a("Mo", 8), a("O", 26)],         
-        [a("H", 2), a("O", 1)]
-    ], BL, BR),
+    parser:parse("MoO3+(C4H9)4NOH=((C4H9)4N)4Mo8O26+H2O", e(L, R)),
+    solver:equation(L, R, BL, BR),
     writeln(BL),
     writeln(BR).
 
